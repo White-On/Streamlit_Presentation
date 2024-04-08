672b4b5d1a91e79cd607323ae0ea9a1cbe82c09b
jofaure <johan.faure@inria.fr>
2024-04-08 14:41:42 +0200

    speed + path following component

--- main_presentation.py
+++ main_presentation.py
@@ -119,10 +119,11 @@ st.markdown(r"> $$R = r_c + r_{nc} + r_{s} + r_p$$")
 st.markdown("We can **eventually** define individual component coefficients to adjust the importance of each component in the final reward. \
             Respectively, we will name them $w_c$, $w_{nc}$, $w_s$ and $w_p$.")
 st.markdown(r"> $$R = w_c \cdot r_c + w_{nc} \cdot r_{nc} + w_s \cdot r_s + w_p \cdot r_p$$")
- 
-st.markdown("> 📌 **Side note**: the components $r_c,r_{nc}, r_{s}$ are based on reward function of **ADD_REF**.\
+
+st.markdown("> 📌 **Side note**: the components $r_c,r_{nc}, r_{s}$ are based on reward function of [[12]](#reference)[[13]](#reference)[[14]](#reference).\
             The component $r_p$ is handcrafted")
 
+st.header("🛤 Path Following component", anchor="path-following-component")
 # st.markdown("The path following score represents the distance between the initially given path to the autonomous \
 #             vehicle (AV) and its future position.\
 #              In this context, the variables $d_p$ and $d$ represent the penalty distance and the distance between \
@@ -132,44 +133,70 @@ st.markdown("> 📌 **Side note**: the components $r_c,r_{nc}, r_{s}$ are based 
 # st.markdown("This formula provides a measure of how closely the AV is following the path, with higher values \
 #             of $R_c$ indicating better adherence to the track.")
 
-st.markdown("First, we define the components $r_p$ responsible for the path-following reward. The path-following \
+st.markdown("First, we define **the components $r_p$ responsible for the path-following reward**. The path-following \
             reward is continuous, aiming to penalize the agent for deviating too far from the path or moving in the \
-            wrong direction. To achieve this, we evaluate the angle error of the agent's trajectory concerning the \
-            next waypoint on the path, denoted as $\\epsilon_{\\theta}$. Recognizing that considering only the angle error may be \
-            insufficient, we also incorporate the distance between the agent and the path, referred to as \
-            $\\epsilon_{path}$. These two values form the basis of the path-following component. We create two separate \
-            versions—one utilizing only $\\epsilon_{\\theta}$ to explore its individual impact on behavior, and another combining \
+            wrong direction. To achieve this, we evaluate **the angle error of the agent's trajectory concerning the \
+            next waypoint on the path**, denoted as $\\epsilon_{\\theta}$. Recognizing that considering only the angle error may be \
+            insufficient, we also incorporate **the distance between the agent and the path**, referred to as \
+            $\\epsilon_{path}$. These two values form the basis of the path-following component. We create *two separate \
+            versions*, one utilizing only $\\epsilon_{\\theta}$ to explore its individual impact on behavior, and another combining \
             both elements to assess their collective influence.") 
 
 st.image(f"{img_path}Path_following_reward.png", use_column_width=True)
 st.markdown("The illustration demonstrates the calculation of $\\epsilon_{\\theta}$. As our agent advances along the path, \
-            we compute the angle between the agent's current heading and the vector directed towards the next \
-            waypoint. $d_w$ represents the distance between the agent and the next waypoint, triggering the agent \
-            to proceed to the subsequent waypoint once it falls within a certain threshold.")
+            we compute **the angle between the agent's current heading and the vector directed towards the next \
+            waypoint**. $d_w$ represents the distance in which, **the agent is \
+            to proceed to the subsequent waypoint once it falls within this threshold**.")
 
 st.image(f"{img_path}path_following_illustration.png", use_column_width=True)
 st.markdown("The illustration demonstrates the calculation of $$\\epsilon_{path}$$ as the distance between the agent and \
-            the path. To incentivize the agent to remain near the path, we model the reward using a sigmoid function \
-            centered around the path, with a variance equal to $\\sigma_{path}$.")
+            the path. To incentivize the agent to remain near the path, we model the reward using a **sigmoid function \
+            centered around the path**, with a zero value threashold equal to $\\sigma_{path}$. $\tau$ is a parameter to \
+            control the steepness of the function.")
 
 st.markdown("The path following reward with only $\\epsilon_{\\theta}$:")
-st.markdown(r"> $$r_p = e^{-\frac{\epsilon_{\theta}}{20}}$$")
+st.markdown(r"> $$r_p = e^{-\frac{\epsilon_{\theta}}{\tau}}$$")
+
+n = 100
+espison_theta = np.linspace(0, 180, n)
+m = st.slider("tau", 1, 50, 20)
+# r_p = 1/(espison_theta + 1)
+angle_component = np.exp(-espison_theta/m) 
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.plot(espison_theta, angle_component)
+ax.set_xlabel('Angle difference [°]')
+ax.set_ylabel('$r_p$')
+ax.set_title('$r_p$ for the angle difference')
+ax.grid(True, linewidth=0.5, linestyle='--')
+ax.xaxis.set_ticks(np.arange(0, 181, 20))
+
+st.pyplot(fig)
+
+
 st.markdown("The path following reward with $\\epsilon_{\\theta}$ and $\\epsilon_{path}$:")
-st.markdown(r"> $$r_p = 1-\frac{2}{1+e^{-d + d_p}}$$")#TODO: add the formula
+st.markdown(r"> $$r_p = e^{-\frac{\epsilon_{\theta}}{\tau}}(1-\frac{2}{1+e^{-\epsilon_{path} + \sigma_{path}}})$$")
 
+def distance_from_path(point, path):
+    a = point - path[0]
+    b = path[1] - path[0]
+    d = np.linalg.norm(a) * np.cos(np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))))
+    normal_point = path[0] + d * b / np.linalg.norm(b)
+    distance_to_rail = np.linalg.norm(point - normal_point)
+    return distance_to_rail
 
+def proximity_reward(distance, penalty_distance):
+    return 1 - 2 / (1 + np.exp(-distance + penalty_distance))
 
-# Plot
 max_distance_display = 20
-d = np.linspace(0, max_distance_display, 100)
-penalty_distance = st.slider("Penalty distance", 0, 20, 10)
-Rc = 1-2 / (1 + np.exp(-d + penalty_distance))
+d = np.linspace(0, max_distance_display, n)
+penalty_distance = st.slider("sigma_path [m]", 0, 20, 10)
+proximity_component = 1-2 / (1 + np.exp(-d + penalty_distance))
 fig , ax = plt.subplots(1, 2 , figsize=(10, 5))
 
-ax[1].plot(d, Rc)
-ax[1].set_xlabel('distance between the future position and the path')
-ax[1].set_ylabel('Reward')
-ax[1].set_title('Reward function for path following')
+ax[1].plot(d, proximity_component)
+ax[1].set_xlabel('Distance between the vehicle position and the path [m]')
+ax[1].set_ylabel('Proximity component')
+# ax[1].set_title('Reward function for path following')
 ax[1].xaxis.set_ticks(np.arange(0, max_distance_display+1, 5))
 ax[1].grid(True, linewidth=0.5, linestyle='--')
 ax[1].set_xlim(0, max_distance_display)
@@ -177,39 +204,141 @@ ax[1].set_ylim(-1.1, 1.1)
 
 path = np.array([[-15, -5], [15, 10]])
 futur_point = np.random.uniform(-15, 15, 2)
+distance_to_rail = distance_from_path(futur_point, path)
+annotate_value = 1 - 2 / (1 + np.exp(-distance_to_rail + penalty_distance))
+ax[1].axvline(distance_to_rail, color='g', linestyle='--')
+ax[1].annotate(f'Distance to path: {distance_to_rail:.2f}\nProximity component: {annotate_value:.2f}',
+                (distance_to_rail + 1, 0.5))
+
+all_points = np.meshgrid(np.linspace(-20, 20, 100), np.linspace(-20, 20, 100))
+print(all_points[0].shape)
 # vector a is start point to futur point
 a = futur_point - path[0]
 # vector b is start point to end point
 b = path[1] - path[0]
 # d is the product of the magnitude of a and cos of the angle between a and b
-d = np.linalg.norm(a) * np.cos(np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))))
-normal_point = path[0] + d * b / np.linalg.norm(b)
-distance_to_rail = np.linalg.norm(futur_point - normal_point)
-Rc = 1 - 2 / (1 + np.exp(-distance_to_rail + penalty_distance))
-ax[1].axvline(distance_to_rail, color='g', linestyle='--')
-ax[1].annotate(f'Distance to path: {distance_to_rail:.2f}\n path following reward: {Rc:.2f}',
-                (distance_to_rail + 1, 0.5))
+e = np.linalg.norm(a) * np.cos(np.arccos(np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))))
+normal_point = path[0] + e * b / np.linalg.norm(b)
+# path = np.array([[-15, -5], [15, 10]])
+distance = np.zeros(all_points[0].shape)
+for i in range(all_points[0].shape[0]):
+    for j in range(all_points[0].shape[1]):
+        distance[i, j] = distance_from_path(np.array([all_points[0][i, j], all_points[1][i, j]]), path)
+reward = proximity_reward(distance, 10)
+reward_img = ax[0].imshow(reward, cmap='plasma', extent=(-20, 20, -20, 20), origin='lower', interpolation='bilinear')
+# reward_img = ax[0].contourf(all_points[0], all_points[1], reward, cmap='plasma')
+# reward_img = ax[0].contour(all_points[0], all_points[1], reward, cmap='plasma')
+# ax[0].clabel(reward_img, inline=True, fontsize=8)
+fig.colorbar(reward_img , ax=ax[0])
 
 ax[0].plot(path[:, 0], path[:, 1], 'r')
-ax[0].scatter(futur_point[0], futur_point[1], c='g', label='Future point')
-ax[0].scatter(path[0, 0], path[0, 1], c='y', label='Start') 
+ax[0].scatter(futur_point[0], futur_point[1], c='g', label='Vehicle position')
+ax[0].scatter(path[0, 0], path[0, 1], c='purple', label='Start') 
 ax[0].scatter(path[1, 0], path[1, 1], c='b', label='End')
-ax[0].scatter(normal_point[0], normal_point[1], c='r', label='Normal point')
+ax[0].scatter(normal_point[0], normal_point[1], c='r', label='Normal to the path')
 ax[0].set_xlim(-20, 20)
 ax[0].set_ylim(-20, 20)
 ax[0].legend(loc='lower right')
+fig.tight_layout(pad=3)
+fig.suptitle('Proximity component', fontsize=16)
+
+st.pyplot(fig)
+
+# r_p = 1/(espison_theta + 1)
+# print(angle_component.shape)
+# print(proximity_component.shape)
+
+# r_c = np.multiply(angle_component, proximity_component.reshape(-1, 1))
+r_c = np.add(angle_component, proximity_component.reshape(-1, 1))
+print(np.min(r_c), np.max(r_c))
+
+fig= plt.figure(figsize=(15, 6))
+ax = plt.subplot(121)
+cs = ax.contourf(espison_theta, d, r_c, cmap='viridis')
+# cs = ax.imshow(r_c, cmap='viridis', extent=(0, 180, 0, max_distance_display), origin='lower')
+fig.colorbar(cs)
+ax.set_xlabel('Angle difference [°]')
+ax.set_ylabel('Distance to path [m]')
+
+# 3D plot
+ax = fig.add_subplot(122, projection='3d')
+espison_theta_mg, d_mg = np.meshgrid(espison_theta, d)
+ax.plot_surface(espison_theta_mg, d_mg, r_c, cmap='viridis')
+ax.set_xlabel('Angle difference [°]')
+ax.set_ylabel('Distance to path [m]')
+ax.set_zlabel('$r_c$')
+ax.view_init(elev=10., azim=30)
+
+fig.tight_layout(pad=0)
+fig.suptitle('$r_c$ for the angle difference and distance to path', fontsize=16, y=1.05)
+
+st.pyplot(fig)
+
+# Speed reward
+st.header("🚀 Speed component", anchor="speed-component")
+st.markdown("The speed reward component is designed to encourage the agent to move at a preferred speed. \
+            The reward is continuous, with higher values indicating that the agent is moving at the desired speed. \
+            The speed reward is calculated using the following formula:")
+st.markdown(r"$$r_s = \left\{ \begin{array}{rcl}1 - \frac{\vec{v^*_{AV}} - \vec{v_{AV}}}{\vec{v^*_{AV}}} & if & 0<\vec{v_{AV}}\leq \vec{v^*_{AV}} \\ -1 & if & \vec{v_{AV}}\leq0 \\ -0.5 & if & \vec{v_{AV}}> \vec{v^*_{AV}}\end{array}\right.$$")
+
+def speed_reward(current_speed, pref_speed):
+    if 0.0 < current_speed <= pref_speed:
+        return 1-(pref_speed - current_speed)/pref_speed
+    elif current_speed > pref_speed:
+        return -0.5
+    elif current_speed <= 0.0:
+        return -1.0
+    
+v_ev = np.linspace(-2, 10, n)
+
+speed_function = np.vectorize(speed_reward)
+pref_speed = st.slider("Preferred speed [m/s]", 0.1, 10.0, 5.0)
+r_speed = speed_function(v_ev, pref_speed)
+
+fig = plt.figure(figsize=(10, 5))
+plt.axvline(pref_speed, color='r', linestyle='--', alpha=0.5, label='Preferred speed')
+plt.plot(v_ev, r_speed)
+plt.xlabel('Linear velocity [m/s]')
+plt.ylabel('Reward')
+plt.title('Speed reward function')
+plt.grid(True, linewidth=0.5, linestyle='--')
+plt.legend()
+
+st.pyplot(fig)
+
+st.markdown("> 📌 **Side note**: The original $r_ {s}$ component[[12]](#reference) was written like the following formula but once plotted, \
+            we can see that the reward seems to reward the agent to go right above the 0 speed.")
+
+st.markdown(r"$$r_s = \left\{ \begin{array}{rcl}\lambda(\vec{v^*_{AV}} - \vec{v_{AV}}) & if & 0<\vec{v_{AV}}\leq \vec{v^*_{AV}} \\ -1 & if & \vec{v_{AV}}\leq0 \\ -0.5 & if & \vec{v_{AV}}> \vec{v^*_{AV}}\end{array}\right. \lambda = \frac{1}{\vec{v^*_{AV}}}$$")
+
+def speed_reward(current_speed, pref_speed):
+    if 0.0 < current_speed <= pref_speed:
+        l = 1/pref_speed # old formula
+        return l * (pref_speed - current_speed)
+    elif current_speed > pref_speed:
+        return -0.5
+    elif current_speed <= 0.0:
+        return -1.0
+    
+v_ev = np.linspace(-2, 10, n)
+
+speed_function = np.vectorize(speed_reward)
+r_speed = speed_function(v_ev, pref_speed)
+
+fig = plt.figure(figsize=(10, 5))
+plt.axvline(pref_speed, color='r', linestyle='--', alpha=0.5, label='Preferred speed')
+plt.plot(v_ev, r_speed)
+plt.xlabel('Linear velocity [m/s]')
+plt.ylabel('Reward')
+plt.title('Speed reward function')
+plt.grid(True, linewidth=0.5, linestyle='--')
+plt.legend()
 
 st.pyplot(fig)
 
 # Motion Safety score
-st.header("🦺 Motion Safety score", anchor="motion-safety-score")
+st.header("🦺 Collision/Near collision Component", anchor="motion-safety-score")
 
-st.markdown("> 📌 **Side note**: This metric is inspired by the paper \
-            [Kabtoul, Maria, Manon Prédhumeau, Anne Spalanzani, Julie Dugdale, et \
-            Philippe Martinet. « How To Evaluate the Navigation of Autonomous Vehicles \
-            Around Pedestrians? » IEEE Transactions on Intelligent Transportation Systems, \
-            2024, 1‑11. https://doi.org/10.1109/TITS.2023.3323662]. In the paper, this \
-            metric was sugested but **not actually used**")
 st.image(f'{img_path}motion_safety_notation.png')
 st.markdown("Motion Safety refers to the assessment of potential risks and hazards associated with vehicle \
             movement, particularly in the presence of pedestrians. Key parameters involved in evaluating \
@@ -347,3 +476,6 @@ st.markdown("[10] Reynolds, T.R. (1987), Stride length and its determinants in \
             humans, early hominids, primates, and mammals. Am. J. Phys. Anthropol., 72: \
             101-115. https://doi.org/10.1002/ajpa.1330720113")
 st.markdown("[11] Renault Zoe's technical sheet https://www.renault.fr/vehicules-electriques/zoe/fiche-technique.html")
+st.markdown("[12] Deshpande, Niranjan, Dominique Vaufreydaz, et Anne Spalanzani. 2021. « Navigation in Urban Environments amongst Pedestrians Using Multi-Objective Deep Reinforcement Learning ». In 2021 IEEE International Intelligent Transportation Systems Conference (ITSC), 923‑28. Indianapolis, IN, USA: IEEE. https://doi.org/10.1109/ITSC48978.2021.9564601.")
+st.markdown("[13] Deshpande, Niranjan, et Anne Spalanzani. 2019. « Deep Reinforcement Learning Based Vehicle Navigation amongst Pedestrians Using a Grid-Based State Representation ». In 2019 IEEE Intelligent Transportation Systems Conference (ITSC), 2081‑86. Auckland, New Zealand: IEEE. https://doi.org/10.1109/ITSC.2019.8917299.")
+st.markdown("[14] Everett, Michael, Yu Fan Chen, et Jonathan P. How. 2021. « Collision Avoidance in Pedestrian-Rich Environments with Deep Reinforcement Learning ». IEEE Access 9: 10357‑77. https://doi.org/10.1109/ACCESS.2021.3050338.")
